62b4288107647cba3bb74f6d6664f4e09506e121
ratuauliya <ratualiya665@gmail.com>
2024-07-12 23:38:19 +0700

    fiturapa

--- app/Http/Controllers/manajemenFilmController.php
+++ app/Http/Controllers/manajemenFilmController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class ManajemenFilmController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $manajemenFilms = ManajemenFilm::all();
